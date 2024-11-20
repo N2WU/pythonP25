8726b02a748df977e7e0206f82c0b4dddd22d953
Nolan Pearce <pearcenolan@gmail.com>
2024-11-20 08:52:20 -0500

    Update print_image.py

--- print_image.py
+++ print_image.py
@@ -17,36 +17,29 @@ def LoadImage():
     img_raw = Image.open('relaxin.png')
     # convert to monochrome bitmap
     img_raw = img_raw.convert('1')
-    #img_raw = np.asarray(img_raw)
+
     # resize to 84...
     base_width = 88
     wpercent = (base_width / float(img_raw.size[0]))
     hsize = int((float(img_raw.size[1]) * float(wpercent)))
     img_adj = img_raw.resize((base_width, hsize), Image.Resampling.LANCZOS)
     img_adj = np.asarray(img_adj)
+
     # is currently an ndarray, should make it a bytearray?
-    # header_list = ['55', '66', '77', '88', '44', '1b', '58', '31', str(int(base_width/8)), str(int(hsize))]
-    header_list =  ['1b', 'X', '1', hex(int(base_width/8)), hex(int(hsize))]
-    #header_str = '\x1b\x58\x31'
-    # imgsize = bytearray(int(base_width/8)) # fix this
-    # imgsize += bytearray(hsize)
-    header = bytearray(b'')
-    for i in range(len(header_list)):
-        header += bytearray(header_list[i], 'utf-8')
-    resize_img = Image.fromarray(img_adj)
-    resize_img.show()
+    # let's give it as an ndarray (each item can be bitized)
+    # 85, 102, 119, 136, 68 #intro header 
+    # [1d, 76, 30] [27, 88, 49] (or 52)
+    header_list = [27, 88, 52, int(base_width/8), int(hsize)]
+    header_list = np.array(header_list)
+ 
+    #resize_img = Image.fromarray(img_adj)
+    #resize_img.show()
     img_flat = np.reshape(img_adj, (1,-1))
     img_flat = img_flat.flatten()
-    #img_byte = bytearray(img_flat)
-    img_out = header
-    for i in range(len(img_flat)):
-        if img_flat[i] == True:
-            img_out += b'\x00'
-        else:
-            img_out += b'\xFF'
+    img_flat = 255*np.array(img_flat)
+    img_out = np.append(header_list,img_flat)
     return img_out
 
-# configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
     port='COM3',
     baudrate=115200,
@@ -60,10 +53,12 @@ print("Serial Open")
 
 # end command: \n
 term_bytes = LoadImage()
-ret_byte = bytearray('\n', 'utf-8')
-term_bytes += ret_byte
+
 ser.reset_output_buffer
-ser.write(term_bytes)
+for i in range(len(term_bytes)):
+    to_ser = bytearray(hex(term_bytes[i]),'utf-8')
+    ser.write(to_ser)
+ser.write(bytearray('\n', 'utf-8'))
 out = ''
 time.sleep(0.5)
 while ser.inWaiting() > 0:
