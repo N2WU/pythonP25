63e2381e4d3da70c6a3c6a939e3a0438711de1c6
N2WU <pearcenolan@gmail.com>
2024-12-06 19:10:00 -0500

    Change MAC address

--- python/webapp_pi/app.py
+++ python/webapp_pi/app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, request
 import time
 import socket
+import yaml
 
 app = Flask(__name__)
 
 def faxprint(name,message):
-    serverMACAddress = '00:08:1b:95:5c:3d'
+    with open('config.yml', 'r') as f:
+       yaml_data = yaml.load(f, Loader=yaml.SafeLoader)
+    serverMACAddress = yaml_data['mac']
     port = 1
     s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
     s.connect((serverMACAddress,port))
     print("Serial Open")
-    #ret_byte = bytearray('\n', 'utf-8')
-    #ser.reset_output_buffer
     if len(name) < 40:
         s.send(bytearray(("from " +  name + ": "), 'utf-8'))
         s.send(bytearray('\n', 'utf-8'))
@@ -39,16 +40,5 @@ def index():
 # there needs to be some form of hook here or in the serial thing
 
 if __name__ == "__main__":
-    # would running code here work?
-    #ser = serial.Serial(
-    #port='COM4',
-    #baudrate=115200,
-    #parity=serial.PARITY_NONE,
-    #stopbits=serial.STOPBITS_TWO,
-    #bytesize=serial.EIGHTBITS,
-    #)
-    #ser.isOpen()
-    #print("Serial Open")
-    #ser.close()
     app.run(debug=True)
 
